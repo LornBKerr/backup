1ddf5d7b08137c17dd952bea7c7280e8448952db
LornBKerr <kerr.larry@gmail.com>
2022-08-27 12:52:58 -0400

    Customized build script for Windows executable

--- install/Windows/build_backup_exe.bat
+++ install/Windows/build_backup_exe.bat
@@ -1,6 +1,15 @@
-# Build the backup executable file
+:: ############################
+:: Build the backup executable file.
+::
+:: Windows 11 Variant
+::
+:: File:       build_backup_executable.bat
+:: Author:     Lorn B Kerr
+:: Copyright:  (c) 2022 Lorn B Kerr
+:: License:    see file LICENSE
+::
+:: ############################
 
-# Windows 11 Variant
 
 pyinstaller --onefile C:\Users\kerrl\development\backup\src\backup.py
 
