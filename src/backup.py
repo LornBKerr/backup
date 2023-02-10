73b66d667950b0cb0b10c13e75238c42f0ea046d
LornBKerr <lornburtkerr@gmail.com>
2023-02-10 11:44:29 -0500

    Expland comments to describe purpose and path of program.

--- src/backup.py
+++ src/backup.py
@@ -1,12 +1,26 @@
 """
-File Backup
-
-Backup the selected file system to the backup storage and to cloud
-storage.
+# File Backup
+#
+# Backup the selected file system to the backup storage and to cloud
+# storage.
+#
+# Part 1: backup to external storage.
+# Backup any new and changed files in the global home directory to the
+# backup directory on an external disk drive. A new file is one that has
+# been added or renamed since the last backup. A changed file is one whose
+# modification time stamp is greater than the backed up file time stamp.
+#
+# Any new directories are created in the backup store and changed files
+# are copied to the store.
+#
+# Part 2 - Not yet implemented
+# After the external storage is updated, backup to cloud storage using
+# the 'restic' program.
+#
 
 File:       backup.py
 Author:     Lorn B Kerr
-Copyright:  (c) 2022 Lorn B Kerr
+Copyright:  (c) 2022, 2023 Lorn B Kerr
 License:    MIT, see file LICENSE
  """
 
