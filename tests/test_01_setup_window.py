3d6a4158d3bc195567789f1253ad47a110ca869c
LornBKerr <lornburtkerr@gmail.com>
2023-02-10 11:59:38 -0500

    Revised handling of IniFileParser and call to SetupWindow.

--- tests/test_01_setup_window.py
+++ tests/test_01_setup_window.py
@@ -18,16 +18,14 @@ from PyQt6.QtWidgets import QApplication, QMainWindow  # QFileDialog, QLineEdit,
 from pytestqt import qtbot
 from setup_window import SetupWindow
 
-config_handler = IniFileParser("backup.ini", "LBKBackup")
+# config_handler = IniFileParser("backup.ini", "backup", config_dir)
+config_handler = IniFileParser("backup.ini", "backup")
 config_file = default_config
 
 
 def test_01_01_SetupWindow():
-    setup = SetupWindow(config_file, config_handler)
-
-
-#    qtbot.addWidget(setup)
-#    assert isinstance(setup, QMainWindow)
+    setup = SetupWindow(config_handler)
+    assert isinstance(setup, SetupWindow)
 
 
 def test_01_02_SetupWindow():
@@ -36,7 +34,7 @@ def test_01_02_SetupWindow():
 
     This is a placeholder until the actual dialog is written.
     """
-    setup = SetupWindow(config_file, config_handler)
+    setup = SetupWindow(config_handler)
     assert isinstance(setup.config, dict)
     assert setup.config["general"]["base_dir"] == os.path.expanduser("~")
     if sys.platform.startswith("linux"):
@@ -44,4 +42,3 @@ def test_01_02_SetupWindow():
     elif sys.platform.startswith("win"):
         assert setup.config["general"]["backup_dir"] == "E:\\Windows11"
     assert setup.config["general"]["last_backup"] <= int(time.time() - 86400)
-    # end test_SetupDialog_02()
